08441b271cd084758146a534d43bb06058dca7e2
gdukes707 <Gdukes707@gmail.com>
2020-09-11 16:11:09 -0700

    Updates

---
+++ precache-manifest.4ebf9a26684be7bf1f535f73d3fa7384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6334ce46020f5ad0a964011e66ec28f5",
+    "url": "/Drink-menu/index.html"
+  },
+  {
+    "revision": "9e04f93c5fd25c3f921c",
+    "url": "/Drink-menu/static/css/main.4ef621b0.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/Drink-menu/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drink-menu/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e04f93c5fd25c3f921c",
+    "url": "/Drink-menu/static/js/main.1f87b472.chunk.js"
+  },
+  {
+    "revision": "1ded79653405b63ca4e0",
+    "url": "/Drink-menu/static/js/runtime-main.4abfc674.js"
+  }
+]);
